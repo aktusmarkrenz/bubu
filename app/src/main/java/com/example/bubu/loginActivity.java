002f918d0c714b42831ce4d22e26a5edea7b9893
yopijay <judan.pauljohn@gmail.com>
2019-12-06 16:34:15 +0800

    Delete files that is not needed!

--- app/src/main/java/com/example/bubu/loginActivity.java
+++ app/src/main/java/com/example/bubu/loginActivity.java
@@ -3,12 +3,37 @@ package com.example.bubu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.reflect.Type;
+
+import mehdi.sakout.fancybuttons.FancyButton;
 
 public class loginActivity extends AppCompatActivity {
+
+    //Fonts
+    Typeface lato_normal;
+    Typeface lato_bold;
+    Typeface monoround;
+
+    //TextView
+    TextView login_txt;
+    TextView forgot_txt;
+    TextView register_txt;
+    TextView register_link;
+
+    //EditText
+    EditText username_txtbox;
+    EditText password_txtbox;
+
+    //FancyButton
+    FancyButton login_btn;
 
 //    ImageView passwordIcon;
 //    EditText password;
@@ -23,13 +48,22 @@ public class loginActivity extends AppCompatActivity {
 
         setContentView(R.layout.login_form);
 
-//        initializeComponents();
+        //Fonts
+        lato_normal = Typeface.createFromAsset(getAssets(), "fonts/Lato-Light.ttf");
+        lato_bold = Typeface.createFromAsset(getAssets(), "fonts/Lato-Bold.ttf");
+        monoround = Typeface.createFromAsset(getAssets(), "fonts/Monoround.otf");
+
+        initializeComponents();
     }
 
 
     private void initializeComponents(){
 //        passwordIcon = findViewById(R.id.password_icon);
 //        password = findViewById(R.id.et_passord);
+
+        login_txt = findViewById(R.id.login_txt);
+        login_txt.setTypeface(lato_normal);
+        login_txt.setTextSize(16);
     }
 
     public void login(View view) {
