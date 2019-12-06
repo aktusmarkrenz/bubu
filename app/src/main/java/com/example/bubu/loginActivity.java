ced6bb339bca717b6913457238187d494a0c2975
yopijay <judan.pauljohn@gmail.com>
2019-12-06 15:08:40 +0800

    Login Form
    Re-design of login form

--- app/src/main/java/com/example/bubu/loginActivity.java
+++ app/src/main/java/com/example/bubu/loginActivity.java
@@ -2,53 +2,56 @@ package com.example.bubu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.media.Image;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class loginActivity extends AppCompatActivity {
 
-    ImageView passwordIcon;
-    EditText password;
+//    ImageView passwordIcon;
+//    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         setContentView(R.layout.login_form);
 
-        initializeComponents();
+//        initializeComponents();
     }
 
 
     private void initializeComponents(){
-        passwordIcon = findViewById(R.id.password_icon);
-        password = findViewById(R.id.et_passord);
+//        passwordIcon = findViewById(R.id.password_icon);
+//        password = findViewById(R.id.et_passord);
     }
 
     public void login(View view) {
         //Toast.makeText(this, "Yeah", Toast.LENGTH_SHORT).show();
-        Intent main = new Intent(this, mainActivity.class);
-        startActivity(main);
+//        Intent main = new Intent(this, mainActivity.class);
+//        startActivity(main);
     }
 
     public void showPassword(View view) {
-        int id = R.drawable.hide;
-        int inputType = InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
-
-        if(password.getInputType() == inputType) {
-            id = R.drawable.view;
-            inputType = InputType.TYPE_CLASS_TEXT;
-        }
-        ((ImageView)view).setImageDrawable(getDrawable(id));
-        password.setInputType(inputType);
-        password.setSelection(password.getText().length());
-    }
-
-    public void userIcon(View view) {
-        Toast.makeText(this, "walang function 'to, bubu", Toast.LENGTH_SHORT).show();
+//        int id = R.drawable.hide;
+//        int inputType = InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
+//
+//        if(password.getInputType() == inputType) {
+//            id = R.drawable.view;
+//            inputType = InputType.TYPE_CLASS_TEXT;
+//        }
+//        ((ImageView)view).setImageDrawable(getDrawable(id));
+//        password.setInputType(inputType);
+//        password.setSelection(password.getText().length());
+//    }
+//
+//    public void userIcon(View view) {
+//        Toast.makeText(this, "walang function 'to, bubu", Toast.LENGTH_SHORT).show();
     }
 }
